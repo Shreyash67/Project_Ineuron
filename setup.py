4963dbdfb9935d6c72a20a7822e1992444a5374d
Shreyash waralkar <waralkarshayu@gmail.com>
2023-11-07 14:44:30 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
+"""HYPEN_E_DOT='-e .'
 
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
@@ -12,7 +12,7 @@ def get_requirements(file_path:str)->List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-    return requirements
+    return requirements"""
 
 setup(
     name = "DiamondPricePrediction",
