9114f5cfbbcc3b22ce2e644852f03f53b8c13019
Shreyash waralkar <waralkarshayu@gmail.com>
2023-12-10 10:47:19 +0530

    File updated

---
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -0,0 +1,6 @@
+class DataTransformation:
+    def __init__(self):
+        pass
+
+    def get_data_transformation(self):
+        pass
---
+++ src/DimondPricePrediction/components/model_trainer.py
@@ -0,0 +1,6 @@
+class ModelTrainer:
+    def __init__(self):
+        pass
+
+    def initiate_model_training(self):
+        pass
