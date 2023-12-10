8a3ec2a1c1e001114db473c3941a9bd51a0d81bd
Shreyash waralkar <waralkarshayu@gmail.com>
2023-12-10 16:31:38 +0530

    logger updated

--- src/DimondPricePrediction/logger.py
+++ src/DimondPricePrediction/logger.py
@@ -13,3 +13,6 @@ LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
 logging.basicConfig(level=logging.INFO,
                     filename=LOG_FILE_PATH,
                     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s")
+
+if __name__=="__main__":
+    logging.info("here again i am testing")
