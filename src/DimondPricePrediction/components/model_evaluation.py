02964b4d195157cac4d399f018e6cb2bd1a3cd7a
Shreyash waralkar <waralkarshayu@gmail.com>
2023-12-17 22:21:06 +0530

    modified code

--- src/DimondPricePrediction/components/model_evaluation.py
+++ src/DimondPricePrediction/components/model_evaluation.py
@@ -2,8 +2,8 @@ import os
 import sys
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from urllib.parse import urlparse
-import mlflow
-import mlflow.sklearn
+# import mlflow
+# import mlflow.sklearn
 import numpy as np
 import pickle
 from src.DimondPricePrediction.utils.utils import load_object
@@ -31,35 +31,35 @@ class ModelEvaluation:
 
         
 
-            mlflow.set_registry_uri("https://dagshub.com/sunny.savita/fsdsmendtoend.mlflow")
+    #         mlflow.set_registry_uri("https://dagshub.com/sunny.savita/fsdsmendtoend.mlflow")
             
-            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+    #         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
             
-            print(tracking_url_type_store)
+    #         print(tracking_url_type_store)
 
 
 
-            with mlflow.start_run():
+    #         with mlflow.start_run():
 
-                predicted_qualities = model.predict(X_test)
+    #             predicted_qualities = model.predict(X_test)
 
-                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+    #             (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
 
-                mlflow.log_metric("rmse", rmse)
-                mlflow.log_metric("r2", r2)
-                mlflow.log_metric("mae", mae)
+    #             mlflow.log_metric("rmse", rmse)
+    #             mlflow.log_metric("r2", r2)
+    #             mlflow.log_metric("mae", mae)
 
 
-                # Model registry does not work with file store
-                if tracking_url_type_store != "file":
+    #             # Model registry does not work with file store
+    #             if tracking_url_type_store != "file":
 
-                    # Register the model
-                    # There are other ways to use the Model Registry, which depends on the use case,
-                    # please refer to the doc for more information:
-                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                    mlflow.sklearn.log_model(model, "model", registered_model_name="ml_model")
-                else:
-                    mlflow.sklearn.log_model(model, "model")
+    #                 # Register the model
+    #                 # There are other ways to use the Model Registry, which depends on the use case,
+    #                 # please refer to the doc for more information:
+    #                 # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+    #                 mlflow.sklearn.log_model(model, "model", registered_model_name="ml_model")
+    #             else:
+    #                 mlflow.sklearn.log_model(model, "model")
 
 
                 
